db921ca7df54e27bd14431c76d3b90c60232a246
Josué Almeida <josue.almeida@gmail.com>
2019-11-08 21:34:03 +0000

    changed name of var constant

--- src/org/academiadecodigo/stringrays/constants/Constants.java
+++ src/org/academiadecodigo/stringrays/constants/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public static final int MIN_NUMBER_OF_PLAYERS = 3;
     public static final int MAX_NUMBER_OF_PLAYERS = 9;
-    public static final int NUMBER_OF_CARDS_IN_PLAYER_HAND = 10;
+    public static final int PLAYER_HAND_SIZE = 10;
     public static final int PORT_NUMBER = 6060;
 }
