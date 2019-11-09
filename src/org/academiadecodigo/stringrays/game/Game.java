0533c1960774b31dfa5a41874441473e32d5567a
Joao Madeira <joaomadeira@outlook.pt>
2019-11-09 13:20:06 +0000

    Game: playedCard, new round and choosewinner added. Player: AlreadyPlayed and chooseWinCard added

--- src/org/academiadecodigo/stringrays/game/Game.java
+++ src/org/academiadecodigo/stringrays/game/Game.java
@@ -1,4 +1,5 @@
 package org.academiadecodigo.stringrays.game;
+
 import org.academiadecodigo.stringrays.game.cards.PopulateDeck;
 import org.academiadecodigo.stringrays.constants.Constants;
 import org.academiadecodigo.stringrays.game.cards.Deck;
@@ -11,41 +12,43 @@ public class Game {
 
     private Deck blackDeck;
     private Deck whiteDeck;
+    private Deck playedCards;
     private Vector<Player> players;
 
     public void init() throws IOException {
 
-
         blackDeck = PopulateDeck.fillDeck(Constants.blackDeck);
         whiteDeck = PopulateDeck.fillDeck(Constants.whiteDeck);
+
         players = new Vector<>();
         createPlayer();
-        players.get(randomFunction(0,players.size())).setCzar(true);
-
-
+        players.get(randomFunction(0, players.size())).setCzar(true);
 
     }
+
     public void start() {
 
 
     }
 
     private void createPlayer() {
+        //instance of new player
         Player newPlayer = new Player();
+
+        //gives ten cards, to the new player
         for (int i = 0; i < Constants.PLAYER_HAND_SIZE; i++) {
             giveCards(newPlayer);
         }
+
+        //adding player to the list of players in game
         players.add(new Player());
     }
 
     private void giveCards(Player player) {
-
-        player.draw(whiteDeck.getCard(randomFunction(0,whiteDeck.getSizeDeck())));
-
+        player.draw(whiteDeck.getCard(randomFunction(0, whiteDeck.getSizeDeck())));
     }
 
-    private void selectCzar() {
-
+    private void setNextCzar() {
         for (Player player : players) {
 
             if (player.isCzar()) {
@@ -69,10 +72,32 @@ public class Game {
 
     }
 
+    private void newRound() {
+        for (Player player : players) {
+
+            if (player.isCzar() || player.alreadyPlayed()) {
+                continue;
+            }
+
+            playedCards.addCard(player.choose(0)); //Need to review this one
+            player.setAlreadyPlayed(true);
+        }
+
+        //after all players, already played
+        chooseWinner(0); //Need to review this one too
+    }
+
+    private void chooseWinner(int index) {
+        for (Player player : players) {
+
+            if (player.isCzar()) {
+                player.chooseWinCard(index, playedCards);
+            }
+        }
+    }
 
 
-    public int randomFunction ( int min, int max  )
-    {
+    public int randomFunction(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
--- src/org/academiadecodigo/stringrays/game/player/Player.java
+++ src/org/academiadecodigo/stringrays/game/player/Player.java
@@ -2,12 +2,16 @@ package org.academiadecodigo.stringrays.game.player;
 
 
 import org.academiadecodigo.stringrays.game.cards.Card;
+import org.academiadecodigo.stringrays.game.cards.Deck;
 import org.academiadecodigo.stringrays.game.cards.PlayerHand;
+
+import java.util.Vector;
 
 public class Player {
 
     private String nickname;
     private boolean isCzar;
+    private boolean alreadyPlayed;
     private int score = 0;
     private PlayerHand hand;
 
@@ -31,12 +35,24 @@ public class Player {
         return hand.getCard(index);
     }
 
+    public Card chooseWinCard(int index, Deck playedCards) {
+        return playedCards.getCard(index);
+    }
+
     public boolean isCzar() {
         return isCzar;
     }
 
     public void setCzar(boolean czar) {
         isCzar = czar;
+    }
+
+    public boolean alreadyPlayed() {
+        return alreadyPlayed;
+    }
+
+    public void setAlreadyPlayed(boolean alreadyPlayed) {
+        this.alreadyPlayed = alreadyPlayed;
     }
 
     public String getNickname() {
