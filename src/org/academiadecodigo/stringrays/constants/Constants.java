e047b64da2b2ab9da0faff4458f795be81bbd793
Josué Almeida <josue.almeida@gmail.com>
2019-11-10 18:28:28 +0000

    Set constantes to their actual value after testing

--- src/org/academiadecodigo/stringrays/constants/Constants.java
+++ src/org/academiadecodigo/stringrays/constants/Constants.java
@@ -3,8 +3,8 @@ package org.academiadecodigo.stringrays.constants;
 public class Constants {
 
     public static final int MIN_NUMBER_OF_PLAYERS = 3;
-    public static final int MAX_NUMBER_OF_PLAYERS = 5; //TODO CHANGE MAX PLAYERS TO 10
-    public static final int SCORE_TO_WIN = 2; //TODO CHANGE TO 8... É OITO, CARAIO!
+    public static final int MAX_NUMBER_OF_PLAYERS = 10; //TODO CHANGE MAX PLAYERS TO 10
+    public static final int SCORE_TO_WIN = 8; //TODO CHANGE TO 8... É OITO, CARAIO!
     public static final int PLAYER_HAND_SIZE = 9;
     public static final int PORT_NUMBER = 6060;
     public static final int CONVERT_PROMPT_VIEW_INDEX = 1;
